204dc2804e1651bcacb75153b696313aa2c594f6
Vineet Kumar <Vineet.Kumar@Radancy.com>
2023-07-31 11:39:19 +0530

    Initial Projects for Web App and Worker Service

---
+++ Containerized-Service/Program.cs
@@ -0,0 +1,10 @@
+using Containerized_Service;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
